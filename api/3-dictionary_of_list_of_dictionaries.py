aec5c991bbd98176508e075254c600ffc07ce027
yene2024 <yeneishla1524@gmail.com>
2024-04-16 12:39:08 -0400

    Adding documentation

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,39 @@
 #!/usr/bin/python3
-
+"""
+This module uses Python to make requests to a REST API.
+q
+It fetches data about a specific employee's tasks
+and prints a summary of the tasks completed and the
+titles of the completed tasks.
+"""
 import json
 import requests
 
-employee_data = requests.get('https://jsonplaceholder.typicode.com/users')
-employee_data_json = employee_data.json()
 
+# Reuet data for users
+users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+
+# Make an empty dictionary
 all_tasks = {}
 
-for user in employee_data_json:
-    user_id = str(user['id'])
+# Loop over all users
+for user in users:
+    # get the user id and name
+    user_id = user['id']
     user_name = user['username']
-    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + user_id)
-    todo_data_json = todo_data.json()
-    task_list = [
-        {
-            'username': user_name,
-            'task': task['title'],
-            'completed': task['completed']
-        } for task in todo_data_json]
+
+    # Get the list
+    todo_list = requests.get(
+        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}').json()
+
+    # Format the task as indicated
+    task_list = [{'username': user_name, 'task': task.get(
+        'title'), 'completed': task.get('completed')} for task in todo_list]
+
+    # Adds the task to the dictionary
     all_tasks[user_id] = task_list
 
+# Export using Json format
 with open('todo_all_employees.json', 'w') as jsonfile:
     json.dump(all_tasks, jsonfile)
 
