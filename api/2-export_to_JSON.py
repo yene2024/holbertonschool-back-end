e417d30ca1b12f3b77d7f53487ac0f48617df9e8
yene2024 <yeneishla1524@gmail.com>
2024-04-16 12:52:07 -0400

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,43 +1,25 @@
 #!/usr/bin/python3
-"""
-Python script to export data in the JSON format
-"""
-
+"""Returns to-do list information about an employee based on their ID."""
 import json
-import requests
-import sys
+from requests import get
+from sys import argv
 
-# Get the employee id from command line
-employee_id = sys.argv[1]
-
-# Get request to api for employee data
-employee_data = requests.get(
-     'https://jsonplaceholder.typicode.com/users/' + employee_id)
-
-# Parse data as json
-employee_data_json = employee_data.json()
-
-# Get employee username from key username
-employee_username = employee_data_json['username']
-
-# Get request to api for todo data
-todo_data = requests.get(
-     'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
-
-# Parse data as json
-todo_data_json = todo_data.json()
-
-# Open a JSON file with the name based on the employee ID and write the data
-with open(f'{employee_id}.json', 'w') as jsonfile:
-    # Dump a dictionary with employee ID as key and a list of tasks as value
-    json.dump({employee_id: [
-        {
-            "task": task['title'],
-            "completed": task['completed'],
-            "username": employee_username
-        } for task in todo_data_json]}, jsonfile)
-
-# Check if the script is being run directly as the main program
 if __name__ == '__main__':
-    # Code inside this block will only run if this script is executed directly
-    pass
+    # Base URL for the JSONPlaceholder API
+    APIurl = "https://jsonplaceholder.typicode.com"
+
+    # Fetch employee data and to-do list from the API
+    employee = get(APIurl + "/users/{}".format(argv[1])).json()
+    to_do_list = get(APIurl + "/todos", params={"userId": argv[1]}).json()
+
+    # Extract the username of the employee
+    username = employee.get("username")
+
+    # Write to-do list data to a JSON file named after the employee ID
+    with open("{}.json".format(argv[1]), "w") as jsonfile:
+        json.dump({argv[1]: [
+            {
+                "task": i.get("title"),
+                "completed": i.get("completed"),
+                "username": username
+            } for i in to_do_list]}, jsonfile)
