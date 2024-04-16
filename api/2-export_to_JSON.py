4298c7ff4c001f08caac8c4081e89fe82db60cd6
yene2024 <yeneishla1524@gmail.com>
2024-04-16 12:36:46 -0400

    Adding documentation

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
+"""
+Python script to export data in the JSON format
+"""
 
 import json
 import requests
 import sys
 
+# Get the employee id from command line
 employee_id = sys.argv[1]
 
-employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                             employee_id)
+# Get request to api for employee data
+employee_data = requests.get(
+     'https://jsonplaceholder.typicode.com/users/' + employee_id)
+
+# Parse data as json
 employee_data_json = employee_data.json()
 
+# Get employee username from key username
 employee_username = employee_data_json['username']
 
-todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' +
-                         employee_id)
+# Get request to api for todo data
+todo_data = requests.get(
+     'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+
+# Parse data as json
 todo_data_json = todo_data.json()
 
-total_todos = str(len(todo_data_json))
-completed_todos = str(sum(1 for task in todo_data_json if task['completed']))
-
+# Open a JSON file with the name based on the employee ID and write the data
 with open(f'{employee_id}.json', 'w') as jsonfile:
+    # Dump a dictionary with employee ID as key and a list of tasks as value
     json.dump({employee_id: [
         {
             "task": task['title'],
             "completed": task['completed'],
             "username": employee_username
         } for task in todo_data_json]}, jsonfile)
+
+# Check if the script is being run directly as the main program
+if __name__ == '__main__':
+    # Code inside this block will only run if this script is executed directly
+    pass
